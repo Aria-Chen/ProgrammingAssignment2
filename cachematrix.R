bbeb3329ce74f1ecefda399baffd0950afac38ec
Aria-Chen <Czzrzclz@163.com>
2015-05-24 19:15:09 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  #solve
+  #n阶  diag(n)#  <-solve(a,diag(n))   <- solve(x)
+  get <- function() x
+  setsolve <- function(solvea) m <<- solvea
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -12,4 +24,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
 }
+
+#numbers = matrix(c(1,2,1,2,3,4,5,1,2,3,4,1,2,8,1,5),nrow=4,ncol=4)
+#cachedNumbers = makeCacheMatrix(numbers)
+#cacheSolve(cachedNumbers)
+#matrix(numbers)
